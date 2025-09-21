135935bdc93c0d4d7df44336a4b52e91564a82bd
Tom Elliott <ipse@paregorios.org>
2025-09-21 11:28:52 -0500

    bump copyright statement in test src file

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,7 +1,7 @@
 #
 # This file is part of pleiades_local
 # by Tom Elliott for the Institute for the Study of the Ancient World
-# (c) Copyright 2023 by New York University
+# (c) Copyright 2023, 2025 by New York University
 # Licensed under the AGPL-3.0; see LICENSE.txt file.
 #
 
